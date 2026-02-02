21fdb933b44321e4c633df6747dfb1aaa899982d
ottoesp <otto.esperson@gmail.com>
2026-02-02 20:20:24 +1100

    Added projects

--- portfolio/src/js/main.js
+++ portfolio/src/js/main.js
@@ -1,5 +1,14 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
-// Import all of Bootstrap’s JS
+// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+document.body.style.display = 'block';
+
+// Prevent card transition flicker on page load
+window.addEventListener('load', () => {
+    document.querySelectorAll('.card').forEach(card => {
+        card.classList.add('loaded');
+    });
+});
